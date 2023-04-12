9645d0b09d57a88d4c65724c2e1e0f49b1253211
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2023-04-13 01:26:43 +0300

    init & done base

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from numpy.random import randint, shuffle
 from numpy import linspace
 import networkx as nx
 import matplotlib.pyplot as plt
+import builtins as __builtin__
+
+def print(message=""):
+    __builtin__.print(message)
+    plt.pause(2)
 
 class Pupil:
     def __init__(self, name, gender, prioList) -> None:
@@ -10,7 +15,7 @@ class Pupil:
         self.k_priorities = len(prioList)
         self.priorities = prioList + ["No one"]
         
-        self.current_priority = -1
+        self.current_priority = 0
         self.queue = []
         self.married = False
         
@@ -27,12 +32,17 @@ class Pupil:
             return self.Name + " has no one"
 
         # return self.Name + " \ncurrent_priority = "+str(self.current_priority)+"married = "+str(self.married)+"\npriorities = "+str(self.priorities) + "\nqueue = "+str(self.queue)
-
+    def selfDescribe(self):
+        return self.Name + " has priorities: " + str(self.priorities)
+    
 def myShuffle(seq):
     seq2 = seq.copy()
     shuffle(seq2)
     return seq2
 
+
+    
+    
 
 def randPeople(names_M, names_G):
     k_priorities_M = [randint(1,len(names_G)) for name in names_M];
@@ -70,13 +80,19 @@ def drawTmpEdges(B, edgesList, pos):
 
 ################# main
 
-# names_M = ["Misha", "Yegor", "Aleksey", "Andey","Pavel"]
-# names_G = ["Marina", "Masha", "Yulia", "Daria", "Lera"]
+names_M = ["Misha", "Yegor", "Aleks", "Andey","Pavel"]
+names_G = ["Marin", "Masha", "Yulia", "Daria", "Lerra"]
 
-names_M,names_G = randNames(50)
+# names_M,names_G = randNames(50)
 mass_M_origin, mass_G_origin = randPeople(names_M, names_G)
 mass_M, mass_G = mass_M_origin, mass_G_origin
 
+for m in mass_M:
+    print(m.selfDescribe())
+print()
+for g in mass_G:
+    print(g.selfDescribe())
+print()
 
 B = nx.Graph()
 B.add_nodes_from(names_M, bipartite=0)
@@ -90,42 +106,90 @@ while not (len(mass_G)==0 or len(mass_M)==0):
     # boys ask
     conEdges = []
     for boy in mass_M:
-        boy.current_priority+=1
-        girl_name = boy.priorities[boy.current_priority]
-        conEdges.append((boy.Name, girl_name))
-        if(girl_name in [g.Name for g in mass_G]):
-            girl = [ g for g in mass_G if g.Name==girl_name][0]
-            girl.queue.append(boy.Name)
-            print(boy.Name + " goes to "+girl.Name)
-    # drawTmpEdges(B,conEdges, pos)
-
+        while boy.current_priority < len(boy.priorities):
+            girl_name = boy.priorities[boy.current_priority]
+            if(girl_name in [g.Name for g in mass_G]):
+                girl = [ g for g in mass_G if g.Name==girl_name][0]
+                if not boy.Name in girl.queue: girl.queue.append(boy.Name)
+                print(boy.Name + " goes to "+girl.Name) # for console
+                conEdges.append((boy.Name, girl_name)) # for graph
+                break
+            else:
+                boy.current_priority+=1
+    drawTmpEdges(B,conEdges, pos) # for graph
+    print()
     # girls answer
     for girl in mass_G:
-        g_priority = -1
-        for best_boy in girl.priorities:
-            g_priority += 1
-            if(best_boy in girl.queue):
-                break
-        else:
-            continue
         
-        boy = [ m for m in mass_M if m.Name==best_boy][0]
-        boy.married = True
+        # define self priority
+        while girl.priorities[girl.current_priority] not in [m.Name for m in mass_M] and girl.current_priority<len(girl.priorities)-2:
+            girl.current_priority+=1
+        print(f"{girl.Name} checkout priority {girl.current_priority}")
         
-        girl.current_priority = g_priority
-        girl.married = True
+        # say 'no'
+        for boy_name in girl.queue:
+            if(boy_name in [m.Name for m in mass_M]):
+                boy = [ m for m in mass_M if m.Name==boy_name][0]
+                
+                if not boy_name in girl.priorities: 
+                    boy.current_priority+=1
+                    girl.queue.pop(girl.queue.index(boy_name)) # correct mistake
+                    print(f"{girl.Name} say 'No' to {boy_name}")
+            
+            else:
+                print("Error")
+            
         
-        marriedList.append((boy.Name, girl.Name))
-        print(f"{girl.Name} say 'Yes' to {boy.Name}, they married")
+        for boy_name in girl.queue:
+            if(boy_name in [m.Name for m in mass_M]):
+                boy = [ m for m in mass_M if m.Name==boy_name][0]
+                
+                
+                if(boy_name == girl.priorities[girl.current_priority]):
+                
+                    # Say yes to him
+                    
+                    boy.married = True
+                    girl.married = True
+                    
+                    marriedList.append((boy.Name, girl.Name))
+                    
+                    print(f"{girl.Name} say 'Yes' to {boy.Name}, they married")
+                    girl.queue.pop(girl.queue.index(boy_name))
+                    
+                    # Say no to other
+                    for boy_name2 in girl.queue:
+                        if(boy_name2 in [m.Name for m in mass_M]): 
+                            boy = [ m for m in mass_M if m.Name==boy_name2][0]
+                            boy.current_priority+=1
+                            print(f"{girl.Name} say 'No' to {boy_name2}")
+                    girl.queue.clear()
+                else:              
+                    print(f"{girl.Name} say 'Wait' to {boy.Name}")
+            
+            
+            else:
+                print("Error")
+                
+                
+
+    print() 
     
+    for m in mass_M:
+        if(m.current_priority==len(m.priorities)-1):
+            print(f"{m.Name} out")
+    for g in mass_G:
+        if(g.current_priority==len(g.priorities)-1):
+            print(f"{g.Name} out")
     
-    mass_M = [m for m in mass_M if m.married==False and m.current_priority<len(m.priorities)-2]
-    mass_G = [g for g in mass_G if g.married==False and g.current_priority<len(g.priorities)-2]
-    # B.add_edges_from(marriedList)
-    # nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
-    # plt.show(block=False)
-    # plt.pause(1)
-    print("p")
+    # deliting married (and out)
+    mass_M = [m for m in mass_M if m.married==False and m.current_priority<len(m.priorities)-1]
+    mass_G = [g for g in mass_G if g.married==False and g.current_priority<len(g.priorities)-1]
+    B.add_edges_from(marriedList)
+    nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
+    plt.show(block=False)
+    plt.pause(1)
+    print("process")
 
 mass_M = mass_M_origin
 mass_G = mass_G_origin
@@ -150,11 +214,11 @@ print("end")
 plt.figure()
 plt.hist([m.current_priority for m in mass_M], bins=linspace(-1-0.5, len(mass_G)+1+0.5      ))
 plt.hist([m.current_priority for m in mass_M if m.current_priority==-1], bins=linspace(-1-0.5, len(mass_G)+1+0.5      ))
-plt.axis([-2,55,0,55])
+# plt.axis([-2,55,0,55])
 plt.figure()
 plt.hist([g.current_priority for g in mass_G],  bins=linspace(-1-0.5, len(mass_M)+1+0.5      ))
 plt.hist([g.current_priority for g in mass_G if g.current_priority==-1], bins=linspace(-1-0.5, len(mass_M)+1+0.5      ))
-plt.axis([-2,55,0,55])
+# plt.axis([-2,55,0,55])
 plt.show()
 
 
