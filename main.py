949439422de9e4aaa1cc54846ba6aa221cc52061
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2023-04-13 02:13:09 +0300

    Drawout improved

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from numpy.random import randint, shuffle
 from numpy import linspace
 import networkx as nx
 import matplotlib.pyplot as plt
-import builtins as __builtin__
 
-def print(message=""):
-    __builtin__.print(message)
-    plt.pause(2)
+secFast = 0.1
+secSlow = 2
+def waitPrint(message="", sec=-1):
+    if(message=="" and sec==-1):
+        sec=secFast
+    if(sec==-1):
+        sec=secSlow
+    print(message)
+    plt.pause(sec)
 
 class Pupil:
     def __init__(self, name, gender, prioList) -> None:
@@ -63,21 +68,38 @@ def randNames(nM,nG=-1):
     names_G = ["Pupil_G"+str(i) for i in range(nG)]
     return names_M,names_G
     
-def drawTmpEdges(B, edgesList, pos):
-    
+def drawTmpEdges(B, pos,  edgesBold, edgesDashed):
     plt.clf()
+    B.add_edges_from(edgesBold)
     nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
-    B.add_edges_from(edgesList)
+    
+    B.add_edges_from(edgesDashed)
     nx.draw_networkx_edges(B,pos=pos,style='dashed')
     plt.show(block=False)
-    plt.pause(1)
+    B.remove_edges_from(edgesDashed)
+
+def blinkEdge(B,pos, edgesBold, edgesDashed1, edgeBlink):
+    edgesDashed = edgesDashed1.copy()
+    if(edgeBlink in edgesDashed):
+        edgesDashed.pop(edgesDashed.index(edgeBlink))
     
-    B.remove_edges_from(edgesList) 
-    plt.clf()
-    nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
-    plt.show(block=False)
-
-
+    for i in range(1,6):
+        plt.clf()
+        B.add_edges_from(edgesBold)
+        nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
+    
+        B.add_edges_from(edgesDashed)
+        nx.draw_networkx_edges(B,pos=pos,style='dashed')
+        plt.pause(secFast)
+        
+        B.add_edge(*edgeBlink)
+        nx.draw_networkx_edges(B,pos=pos,style='dashed')
+        B.remove_edges_from(edgesDashed + [edgeBlink])
+        plt.pause(secFast)
+    
+    
+    
+     
 ################# main
 
 names_M = ["Misha", "Yegor", "Aleks", "Andey","Pavel"]
@@ -88,11 +110,11 @@ mass_M_origin, mass_G_origin = randPeople(names_M, names_G)
 mass_M, mass_G = mass_M_origin, mass_G_origin
 
 for m in mass_M:
-    print(m.selfDescribe())
-print()
+    waitPrint(m.selfDescribe(),secFast)
+waitPrint(sec=secFast)
 for g in mass_G:
-    print(g.selfDescribe())
-print()
+    waitPrint(g.selfDescribe(),secFast)
+waitPrint(sec=secFast)
 
 B = nx.Graph()
 B.add_nodes_from(names_M, bipartite=0)
@@ -102,29 +124,36 @@ pos.update((node, (1, index)) for index, node in enumerate(names_M))
 pos.update((node, (2, index)) for index, node in enumerate(names_G))
 
 marriedList = []
+dayNum = 0
 while not (len(mass_G)==0 or len(mass_M)==0):
+    dayNum+=1
+    waitPrint(f"process, day {dayNum}")
+    
     # boys ask
-    conEdges = []
+    tmpEdges = []
     for boy in mass_M:
         while boy.current_priority < len(boy.priorities):
             girl_name = boy.priorities[boy.current_priority]
             if(girl_name in [g.Name for g in mass_G]):
                 girl = [ g for g in mass_G if g.Name==girl_name][0]
                 if not boy.Name in girl.queue: girl.queue.append(boy.Name)
-                print(boy.Name + " goes to "+girl.Name) # for console
-                conEdges.append((boy.Name, girl_name)) # for graph
+                tmpEdges.append((boy.Name, girl_name)) # for graph
+                drawTmpEdges(B,pos,marriedList,tmpEdges)
+                waitPrint(boy.Name + " goes to "+girl.Name) # for console
                 break
             else:
                 boy.current_priority+=1
-    drawTmpEdges(B,conEdges, pos) # for graph
-    print()
+        if(boy.current_priority == len(boy.priorities)-1):
+            waitPrint(f"{boy.Name} out")
+    drawTmpEdges(B,pos,marriedList, tmpEdges) # for graph
+    waitPrint()
     # girls answer
     for girl in mass_G:
         
         # define self priority
         while girl.priorities[girl.current_priority] not in [m.Name for m in mass_M] and girl.current_priority<len(girl.priorities)-2:
             girl.current_priority+=1
-        print(f"{girl.Name} checkout priority {girl.current_priority}")
+        waitPrint(f"{girl.Name} checkout priority {girl.current_priority}")
         
         # say 'no'
         for boy_name in girl.queue:
@@ -134,10 +163,12 @@ while not (len(mass_G)==0 or len(mass_M)==0):
                 if not boy_name in girl.priorities: 
                     boy.current_priority+=1
                     girl.queue.pop(girl.queue.index(boy_name)) # correct mistake
-                    print(f"{girl.Name} say 'No' to {boy_name}")
+                    tmpEdges.pop(tmpEdges.index((boy.Name, girl.Name)))
+                    drawTmpEdges(B,pos,marriedList, tmpEdges)
+                    waitPrint(f"{girl.Name} say 'No' to {boy_name}")
             
             else:
-                print("Error")
+                waitPrint("Error")
             
         
         for boy_name in girl.queue:
@@ -153,8 +184,8 @@ while not (len(mass_G)==0 or len(mass_M)==0):
                     girl.married = True
                     
                     marriedList.append((boy.Name, girl.Name))
-                    
-                    print(f"{girl.Name} say 'Yes' to {boy.Name}, they married")
+                    drawTmpEdges(B,pos,marriedList, tmpEdges)
+                    waitPrint(f"{girl.Name} say 'Yes' to {boy.Name}, they married")
                     girl.queue.pop(girl.queue.index(boy_name))
                     
                     # Say no to other
@@ -162,25 +193,29 @@ while not (len(mass_G)==0 or len(mass_M)==0):
                         if(boy_name2 in [m.Name for m in mass_M]): 
                             boy = [ m for m in mass_M if m.Name==boy_name2][0]
                             boy.current_priority+=1
-                            print(f"{girl.Name} say 'No' to {boy_name2}")
+                            
+                            tmpEdges.pop(tmpEdges.index((boy.Name, girl.Name)))
+                            drawTmpEdges(B,pos,marriedList, tmpEdges)
+                            waitPrint(f"{girl.Name} say 'No' to {boy_name2}")
                     girl.queue.clear()
                 else:              
-                    print(f"{girl.Name} say 'Wait' to {boy.Name}")
+                    waitPrint(f"{girl.Name} say 'Wait' to {boy.Name}")
+                    blinkEdge(B,pos,marriedList,tmpEdges,(boy.Name,girl.Name))
             
             
             else:
-                print("Error")
+                waitPrint("Error")
                 
                 
 
-    print() 
+    waitPrint() 
     
     for m in mass_M:
         if(m.current_priority==len(m.priorities)-1):
-            print(f"{m.Name} out")
+            waitPrint(f"{m.Name} out")
     for g in mass_G:
         if(g.current_priority==len(g.priorities)-1):
-            print(f"{g.Name} out")
+            waitPrint(f"{g.Name} out")
     
     # deliting married (and out)
     mass_M = [m for m in mass_M if m.married==False and m.current_priority<len(m.priorities)-1]
@@ -189,7 +224,7 @@ while not (len(mass_G)==0 or len(mass_M)==0):
     nx.draw(B, with_labels=True, font_weight='bold', pos=pos,node_size=2000)
     plt.show(block=False)
     plt.pause(1)
-    print("process")
+    
 
 mass_M = mass_M_origin
 mass_G = mass_G_origin
@@ -204,9 +239,9 @@ for g in mass_G:
 
 
 
-print("Results:")
-print(mass_M_origin)
-print(mass_G_origin)
+waitPrint("Results:",secFast)
+waitPrint(mass_M_origin,secFast)
+waitPrint(mass_G_origin,secFast)
 print("end")
 # plt.show()
 
